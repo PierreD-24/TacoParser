53811e94c52639205a1f1b7e13a0242c11f653a2
Pierre Dahrouj <pdahrouj7@gmail.com>
2024-10-15 10:31:42 -0700

    Completed taco parser exercise

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -18,35 +18,45 @@
             if (cells.Length < 3)
             {
                 // Log error message and return null
+                logger.LogWarning("Your array is less than 3");
                 return null; 
             }
 
             // TODO: Grab the latitude from your array at index 0
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
+            var latitude = double.Parse(cells[0]);
             
             
             // TODO: Grab the longitude from your array at index 1
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
+            var longitude = double.Parse(cells[1]);
             
             
             // TODO: Grab the name from your array at index 2
+            string name = cells[2];
             
 
             // TODO: Create a TacoBell class
             // that conforms to ITrackable
             
             // TODO: Create an instance of the Point Struct
-            // TODO: Set the values of the point correctly (Latitude and Longitude) 
+            // TODO: Set the values of the point correctly (Latitude and Longitude)
+            var tacoBellPoint = new Point();
+            tacoBellPoint.Latitude = latitude;
+            tacoBellPoint.Longitude = longitude;
 
             // TODO: Create an instance of the TacoBell class
             // TODO: Set the values of the class correctly (Name and Location)
+            var tacoBell = new TacoBell();
+            tacoBell.Name = name;
+            tacoBell.Location = tacoBellPoint;
 
             // TODO: Then, return the instance of your TacoBell class,
             // since it conforms to ITrackable
 
-            return null;
+            return tacoBell;
         }
     }
 }
